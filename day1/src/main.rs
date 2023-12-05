8de10b45758a71e01449f0a080ff12e179e049cb
Abhinav Garg <ag90@illinois.edu>
2023-12-05 16:00:24 -0600

    challenge completed!

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,20 @@
+use std::{fs::{self, File}, io::Write};
+
 fn main() {
-    println!("Hello, world!");
+    let filename = "input.txt";
+    let file_contents = fs::read_to_string(filename).expect("Error in reading file");
+    let lines = file_contents.split("\n");
+    let mut running_sum: u32 = 0;
+    for line in lines {
+        let nums: Vec<u32> = line
+            .chars()
+            .filter(|character| ((*character as i32) > 0x30 && (*character as i32) < 0x3A))
+            .map(|char| char.to_digit(10).unwrap())
+            .collect();
+        running_sum += (nums[0])*10 + nums[nums.len()-1];; 
+
+    }
+    let mut file = File::create("output.txt").unwrap();
+    file.write_fmt(format_args!("{}", running_sum)).unwrap();
+
 }
